32d4e23648eb6384f103f540e5370c4e7a6f35b3
svobodamatej24 <matej.svob36@gmail.com>
2026-04-27 16:51:00 +0200

    soubor pipe

--- FlappyBird/src/FB/Pipe.java
+++ FlappyBird/src/FB/Pipe.java
@@ -5,5 +5,26 @@ import java.awt.*;
 public class Pipe {
     int x;
     int y;
+    int width = 64;
+    int height = 512;
+    Image image;
+    int rychlostX = -4;
 
+    public Pipe(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+    }
+
+    public void move() {
+        x -= rychlostX;
+    }
+
+    public void draw(Graphics g) {
+        if (image != null) {
+            g.drawImage(image, x, y, width, height, null);
+        } else {
+            g.setColor(Color.BLACK);
+            g.fillRect(x, y, width, height);
+        }
+    }
 }
